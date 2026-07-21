ef30782c9754cd6a6ac102f541792550b05b40de
tommy <tommy.miao@buttontech.com>
2026-07-21 11:43:50 +0800

    Initial commit

---
+++ custom_network_sdk/custom_network_sdk.swift
@@ -0,0 +1,9 @@
+//
+//  custom_network_sdk.swift
+//  custom_network_sdk
+//
+//  Created by 纽扣 on 2026/7/21.
+//
+
+import Foundation
+
---
+++ custom_network_sdkTests/custom_network_sdkTests.swift
@@ -0,0 +1,17 @@
+//
+//  custom_network_sdkTests.swift
+//  custom_network_sdkTests
+//
+//  Created by 纽扣 on 2026/7/21.
+//
+
+import Testing
+@testable import custom_network_sdk
+
+struct custom_network_sdkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
